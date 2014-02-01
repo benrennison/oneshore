a61b884e0810cd2ba07fc9878b854587f42a38ba
Ben Rennison <ben.rennison@gmail.com>
2014-02-01 17:46:30 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Oneshore::Application.config.secret_key_base = '9a5e1b9bf7b7dae46b4d5433beb3f1ea7237b80ad5574a5f98924545039c8a2ebdc3a08e9bd2931207ccbdd08672890ea7f21e906bbd9adc4cd6578333f9fa23'
